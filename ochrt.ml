ad4acddfffddd077f9b578d019c3d36aa9ac330d
spinclick <spinclick@users.noreply.github.com>
2016-04-11 22:55:52 -0400

    Add octal and hex-index output

--- ochrt.ml
+++ ochrt.ml
@@ -11,21 +11,25 @@ let print_binary num =
     print_char (if ((num lsr i) mod 2) = 0 then '0' else '1')
  done;;
 
-print_endline "indx int binary   hex  char";
+print_endline "__indx___  int chr  hex  binary   oct";
 
 let byte_count = ref 0 in
 try
   while true do
     let byte = input_byte f_in in
       (*print_int !byte_count;*)
-      Printf.printf "%-4d" !byte_count;
+      Printf.printf "%-3d " !byte_count;
+      Printf.printf "%+2s%-2X" "0x" !byte_count;
       byte_count := succ !byte_count;
       print_char ' ';
+      print_char '|';
+      print_char ' ';
       Printf.printf "%-3d " byte;
+      Printf.printf "%-5s" (escaped (chr byte));
+      Printf.printf "0x%02X " byte;
       print_binary byte;
       print_char ' ';
-      Printf.printf "0x%02X " byte;
-      print_string (escaped (chr byte));
+      Printf.printf "%-3o " byte;
       print_endline "";
   done
 with End_of_file ->
